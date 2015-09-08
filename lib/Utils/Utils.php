c9e5bbd0fb8be77358fefd7ead5cf6bed75d7637
ddolcimascolo <ddolcimascolo@linagora.com>
2015-09-08 11:30:46 +0200

    OR-1689 Fixed linters.

--- lib/Utils/Utils.php
+++ lib/Utils/Utils.php
@@ -4,16 +4,16 @@ namespace ESN\Utils;
 
 class Utils {
 
-  static function firstEmailAddress($user) {
-      if (array_key_exists('accounts', $user)) {
-          foreach ($user['accounts'] as $account) {
-              if ($account['type'] === 'email') {
-                  return $account['emails'][0];
-              }
-          }
-      }
+    static function firstEmailAddress($user) {
+        if (array_key_exists('accounts', $user)) {
+            foreach ($user['accounts'] as $account) {
+                if ($account['type'] === 'email') {
+                    return $account['emails'][0];
+                }
+            }
+        }
 
-      return null;
-  }
+        return null;
+    }
 
 }

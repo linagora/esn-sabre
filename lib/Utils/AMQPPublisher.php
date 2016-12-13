b502d1d1fdb1cc97261a3651cf3eea1dbe8d34ef
Graham Crosmarie <gcrosmarie@linagora.com>
2016-12-13 14:46:27 +0100

    CAL-443 fix linters

--- lib/Utils/AMQPPublisher.php
+++ lib/Utils/AMQPPublisher.php
@@ -3,18 +3,18 @@ namespace ESN\Utils;
 
 class AMQPPublisher {
 
-  function __construct($channel) {
-      $this->channel = $channel;
+    function __construct($channel) {
+        $this->channel = $channel;
 
-      if (!$this->channel) {
-        error_log('Please provide a non null channel when creating a RabbitmqClient');
-      }
-  }
+        if (!$this->channel) {
+            error_log('Please provide a non null channel when creating a RabbitmqClient');
+        }
+    }
 
-  function publish($topic, $message) {
-      $this->channel->exchange_declare($topic, 'fanout', true, false, false); //this correspond to passive: true, durable: false, auto_delete: false
-      $msg = new \PhpAmqpLib\Message\AMQPMessage(utf8_encode($message));
+    function publish($topic, $message) {
+        $this->channel->exchange_declare($topic, 'fanout', true, false, false); //this correspond to passive: true, durable: false, auto_delete: false
+        $msg = new \PhpAmqpLib\Message\AMQPMessage(utf8_encode($message));
 
-      $this->channel->basic_publish($msg, $topic);
-  }
+        $this->channel->basic_publish($msg, $topic);
+    }
 }

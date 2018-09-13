f54a6cbe653847a09f17289718497f6bf3585967
Aymane KHOUAJI <akhouaji@linagora.com>
2018-09-13 10:12:29 +0200

    Revert "Merge branch '#1385' into 'master'"
    This reverts merge request !125

--- lib/CalDAV/Backend/Esn.php
+++ lib/CalDAV/Backend/Esn.php
@@ -7,10 +7,15 @@ class Esn extends Mongo {
     const EVENTS_URI = 'events';
 
     function getCalendarsForUser($principalUri) {
-        $principalExploded = explode('/', $principalUri);
-        
-        parent::createDefaultCalendar($principalUri, $principalExploded[2]);
         $calendars = parent::getCalendarsForUser($principalUri);
+
+        if (count($calendars) == 0) {
+            // No calendars yet, inject our default calendars
+            $principalExploded = explode('/', $principalUri);
+            parent::createCalendar($principalUri, $principalExploded[2], []);
+
+            $calendars = parent::getCalendarsForUser($principalUri);
+        }
 
         return $calendars;
     }

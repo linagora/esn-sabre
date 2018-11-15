2f750effe4667519b1c0ae73250d2dd78d761c6f
Tuan LE CONG <tlcong@linagora.com>
2018-11-15 13:54:24 +0700

    linagora/lgs/openpaas/linagora.esn.contact#445 DAVRoid: Do not return address book if the query book ID is not owner ID

--- tests/CardDAV/MobileRequestPluginTest.php
+++ tests/CardDAV/MobileRequestPluginTest.php
@@ -77,4 +77,154 @@ class MobileRequestPluginTest extends \ESN\DAV\ServerMock {
         // Normal address book
         $this->assertContains('Book 1 - Roberto Carlos', $displayNames);
     }
+
+    function testAfterPropFindWithGroupAddressBook() {
+        $DOMAIN_ID = '54b64eadf6d7d8e41d263e7e';
+        $USER_ID = '54b64eadf6d7d8e41d263e8f';
+
+        $this->esndb->domains->insertOne([
+            '_id' => new \MongoDB\BSON\ObjectId($DOMAIN_ID),
+            'name' => 'test',
+            'administrators' => [
+                [
+                    'user_id' => $USER_ID
+                ]
+            ]
+        ]);
+
+        $this->esndb->users->insertOne([
+            '_id' => new \MongoDB\BSON\ObjectId($USER_ID),
+            'firstname' => 'foo',
+            'lastname' => 'bar',
+            'accounts' => [
+                [
+                    'type' => 'email',
+                    'emails' => [
+                      'foobar@lng.com'
+                    ]
+                ]
+                    ],
+            'domains' => [ [ 'domain_id' => $DOMAIN_ID ] ]
+        ]);
+
+        $this->carddavBackend->createAddressBook('principals/domains/' . $DOMAIN_ID, 'gab', [ '{DAV:}acl' => [ '{DAV:}read' ] ]);
+
+        $this->authBackend->setPrincipal('principals/users/' . $USER_ID);
+
+        $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
+            'REQUEST_METHOD'    => 'PROPFIND',
+            'HTTP_CONTENT_TYPE' => 'application/xml',
+            'HTTP_ACCEPT'       => 'application/xml',
+            'HTTP_USER_AGENT'   => 'DAVdroid/1.10.1.1-ose (2/13/18; dav4android; okhttp3) Android/8.1.0',
+            'REQUEST_URI'       => '/addressbooks/' . $USER_ID,
+        ));
+
+        $request->setBody('<?xml version="1.0" encoding="utf-8" ?>
+            <D:propfind xmlns:D="DAV:">
+                <D:prop>
+                    <D:displayname/>
+                    <D:resourcetype/>
+                </D:prop>
+            </D:propfind>'
+        );
+
+        $response = $this->request($request);
+
+        $this->assertEquals($response->status, 207);
+
+        $propFindXml = $this->server->xml->expect('{DAV:}multistatus', $response->getBodyAsString());
+        $xmlResponses = $propFindXml->getResponses();
+
+        $displayNames = [];
+
+        foreach($xmlResponses as $index => $xmlResponse) {
+            $responseProps = $xmlResponse->getResponseProperties();
+            $resourceType = isset($responseProps[200]['{DAV:}resourcetype']) ? $responseProps[200]['{DAV:}resourcetype'] : null;
+            
+            if (isset($resourceType) && ($resourceType->is("{urn:ietf:params:xml:ns:carddav}addressbook"))) {
+                $displayNames[] = $responseProps[200]['{DAV:}displayname'];
+            }
+        }
+
+        $this->assertCount(2, $displayNames);
+
+        // My default address books
+        $this->assertContains('My Collected Contacts', $displayNames);
+        $this->assertContains('My Contacts', $displayNames);
+    }
+
+    function testAfterPropFindWithGroupAddressBookWhenBookIdIsOwnerId() {
+        $DOMAIN_ID = '54b64eadf6d7d8e41d263e7e';
+        $USER_ID = '54b64eadf6d7d8e41d263e8f';
+
+        $this->esndb->domains->insertOne([
+            '_id' => new \MongoDB\BSON\ObjectId($DOMAIN_ID),
+            'name' => 'test',
+            'administrators' => [
+                [
+                    'user_id' => $USER_ID
+                ]
+            ]
+        ]);
+
+        $this->esndb->users->insertOne([
+            '_id' => new \MongoDB\BSON\ObjectId($USER_ID),
+            'firstname' => 'foo',
+            'lastname' => 'bar',
+            'accounts' => [
+                [
+                    'type' => 'email',
+                    'emails' => [
+                      'foobar@lng.com'
+                    ]
+                ]
+                    ],
+            'domains' => [ [ 'domain_id' => $DOMAIN_ID ] ]
+        ]);
+
+        $this->carddavBackend->createAddressBook('principals/domains/' . $DOMAIN_ID, 'gab', [
+            '{DAV:}acl'         => [ '{DAV:}read' ],
+            '{DAV:}displayname' => 'Domain address book'
+        ]);
+
+        $this->authBackend->setPrincipal('principals/users/' . $USER_ID);
+
+        $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
+            'REQUEST_METHOD'    => 'PROPFIND',
+            'HTTP_CONTENT_TYPE' => 'application/xml',
+            'HTTP_ACCEPT'       => 'application/xml',
+            'HTTP_USER_AGENT'   => 'DAVdroid/1.10.1.1-ose (2/13/18; dav4android; okhttp3) Android/8.1.0',
+            'REQUEST_URI'       => '/addressbooks/' . $DOMAIN_ID,
+        ));
+
+        $request->setBody('<?xml version="1.0" encoding="utf-8" ?>
+            <D:propfind xmlns:D="DAV:">
+                <D:prop>
+                    <D:displayname/>
+                    <D:resourcetype/>
+                </D:prop>
+            </D:propfind>'
+        );
+
+        $response = $this->request($request);
+
+        $this->assertEquals($response->status, 207);
+
+        $propFindXml = $this->server->xml->expect('{DAV:}multistatus', $response->getBodyAsString());
+        $xmlResponses = $propFindXml->getResponses();
+
+        $displayNames = [];
+
+        foreach($xmlResponses as $index => $xmlResponse) {
+            $responseProps = $xmlResponse->getResponseProperties();
+            $resourceType = isset($responseProps[200]['{DAV:}resourcetype']) ? $responseProps[200]['{DAV:}resourcetype'] : null;
+            
+            if (isset($resourceType) && ($resourceType->is("{urn:ietf:params:xml:ns:carddav}addressbook"))) {
+                $displayNames[] = $responseProps[200]['{DAV:}displayname'];
+            }
+        }
+
+        $this->assertCount(1, $displayNames);
+        $this->assertEquals('Domain address book - test', $displayNames[0]);
+    }
 }

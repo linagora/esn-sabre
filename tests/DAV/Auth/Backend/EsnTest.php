33a8e9c1a2c3dc3f342457e60060df8e4c477652
Bastien Roucariès <rouca@debian.org>
2025-08-05 17:16:02 +0200

    Update sabre
    - Switched to using WildcardEmitter (Sabre/Dav #896)
    - need _SERVER to be populated for test (sabre/http)
    
    Signed-off-by: Bastien Roucariès <rouca@debian.org>

--- tests/DAV/Auth/Backend/EsnTest.php
+++ tests/DAV/Auth/Backend/EsnTest.php
@@ -86,6 +86,8 @@ class EsnTest extends \PHPUnit_Framework_TestCase {
         $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
             'PHP_AUTH_USER' => 'username',
             'PHP_AUTH_PW' => 'password',
+            'REQUEST_URI' => '/',
+            'REQUEST_METHOD' => 'GET',
         ));
         $response = new \Sabre\HTTP\Response();
 
@@ -110,6 +112,8 @@ class EsnTest extends \PHPUnit_Framework_TestCase {
         $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
             'PHP_AUTH_USER' => 'username',
             'PHP_AUTH_PW' => 'password',
+            'REQUEST_URI' => '/foo/bar',
+            'REQUEST_METHOD' => 'GET',
         ));
         $response = new \Sabre\HTTP\Response();
 
@@ -136,6 +140,8 @@ class EsnTest extends \PHPUnit_Framework_TestCase {
         $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
             'PHP_AUTH_USER' => 'username',
             'PHP_AUTH_PW' => 'password',
+            'REQUEST_URI' => '/foo/bar',
+            'REQUEST_METHOD' => 'GET',
         ));
         $response = new \Sabre\HTTP\Response();
 
@@ -162,6 +168,8 @@ class EsnTest extends \PHPUnit_Framework_TestCase {
         $request = \Sabre\HTTP\Sapi::createFromServerArray(array(
             'PHP_AUTH_USER' => 'username',
             'PHP_AUTH_PW' => 'password',
+            'REQUEST_URI' => '/foo/bar',
+            'REQUEST_METHOD' => 'GET',
         ));
         $response = new \Sabre\HTTP\Response();
 

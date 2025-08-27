37034eff494b7a2d31cb5128f50282a08c29f7a5
Benoit TELLIER <btellier@linagora.com>
2025-08-27 19:16:00 +0200

    fixup! fixup! fixup! fixup! Jenkinsfile

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker build -t esn-sabre-ldap-test -f Dockerfile.ldap .
 docker build -t esn_sabre_test .
 docker compose -f docker-compose.test.yaml run --rm esn_test
 docker compose -f docker-compose.test.yaml stop

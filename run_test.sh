b24a121d9fb41333fa369cb4aee9e2a80e702bc4
Benoit TELLIER <btellier@linagora.com>
2025-08-27 19:26:22 +0200

    Jenkinsfile (#39)

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
+docker build -t esn-sabre-ldap-test -f Dockerfile.ldap .
 docker build -t esn_sabre_test .
+
 docker compose -f docker-compose.test.yaml run --rm esn_test
+exit_code=$?
+
+# Always clean
 docker compose -f docker-compose.test.yaml stop
 docker compose -f docker-compose.test.yaml rm --force
+
+exit $exit_code

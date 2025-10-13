36addc6c5660ca50264d24b64df06045e02c78b6
Benoit TELLIER <btellier@linagora.com>
2025-10-13 14:48:54 +0200

    Run integration tests upon Sabre build

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+cleanup() {
+  echo "Cleaning up..."
+  rm -rf it-tests
+  docker compose -f docker-compose.test.yaml down --volumes --remove-orphans || true
+}
+trap cleanup EXIT  # finally: sera exécuté *quoi qu'il arrive*
+
 # Pre Cleanup
 docker compose -f docker-compose.test.yaml down --volumes --remove-orphans || true
 
@@ -8,9 +15,20 @@ docker build -t esn-sabre-ldap-test -f Dockerfile.ldap .
 docker build -t esn_sabre_test .
 
 docker compose -f docker-compose.test.yaml run --rm esn_test
-exit_code=$?
+exit_code_1=$?
 
-# Post Cleanup
-docker compose -f docker-compose.test.yaml down --volumes --remove-orphans || true
+if [ $exit_code_1 -ne 0 ]; then
+  exit 1
+fi
 
-exit $exit_code
+(
+  git clone https://github.com/linagora/twake-calendar-integration-tests.git it-tests
+  cd it-tests
+  bash pre-build.sh esn_sabre_test
+  mvn clean install -Dtest=com.linagora.dav.sabrev4.**
+)
+exit_code_2=$?
+
+if [ $exit_code_2 -ne 0 ]; then
+  exit 1
+fi

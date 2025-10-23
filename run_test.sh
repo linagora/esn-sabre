2a2b943fd43110782808d34e1b1c3be3448b25b0
vttran <vttran@linagora.com>
2025-10-23 15:12:08 +0700

    TODO: revert — temporary change for CI test

--- run_test.sh
+++ run_test.sh
@@ -61,7 +61,7 @@ if [ "$SKIP_JAVA" = true ]; then
 fi
 
 (
-  git clone https://github.com/linagora/twake-calendar-integration-tests.git it-tests
+  git clone -b esn-sabre-issue-152 --single-branch https://github.com/vttranlina/twake-calendar-integration-tests.git it-tests
   cd it-tests
   bash pre-build.sh esn_sabre_test
   mvn clean install -Dtest=com.linagora.dav.sabrev4.**

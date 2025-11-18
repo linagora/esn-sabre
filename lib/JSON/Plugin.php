0ab01ce907ef4496a87ee4150d4e31dc599e590b
Benoit TELLIER <btellier@linagora.com>
2025-11-18 16:45:06 +0100

    ISSE-223 Normalise source URI

--- lib/JSON/Plugin.php
+++ lib/JSON/Plugin.php
@@ -753,6 +753,15 @@ class Plugin extends \Sabre\CalDAV\Plugin {
         if (isset($subprops['{http://calendarserver.org/ns/}source'])) {
             $sourcePath = $subprops['{http://calendarserver.org/ns/}source']->getHref();
 
+            // If it starts with "http://", remove it
+            if (str_starts_with($sourcePath, 'http://')) {
+                $sourcePath = substr($sourcePath, strlen('http://'));
+            }
+            // If it starts with "/", remove the leading slash
+            if (str_starts_with($sourcePath, '/')) {
+                $sourcePath = substr($sourcePath, 1);
+            }
+
             error_log("Source path: $sourcePath");
             if (!$this->server->tree->nodeExists($sourcePath)) {
                 return null;

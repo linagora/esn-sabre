37acd6f64ec9583274434417cdd0340c5ce590e2
Benoit TELLIER <btellier@linagora.com>
2025-12-09 16:16:20 +0100

    Extract CalendarObjectService

--- lib/CalDAV/Backend/Mongo.php
+++ lib/CalDAV/Backend/Mongo.php
@@ -15,6 +15,7 @@ use ESN\CalDAV\Backend\Service\CalendarDataNormalizer;
 use ESN\CalDAV\Backend\Service\SubscriptionService;
 use ESN\CalDAV\Backend\Service\SchedulingService;
 use ESN\CalDAV\Backend\Service\CalendarService;
+use ESN\CalDAV\Backend\Service\CalendarObjectService;
 
 #[\AllowDynamicProperties]
 class Mongo extends \Sabre\CalDAV\Backend\AbstractBackend implements
@@ -38,6 +39,7 @@ class Mongo extends \Sabre\CalDAV\Backend\AbstractBackend implements
     protected $subscriptionService;
     protected $schedulingService;
     protected $calendarService;
+    protected $calendarObjectService;
 
     public $propertyMap = [
         '{DAV:}displayname' => 'displayname',
@@ -86,6 +88,10 @@ class Mongo extends \Sabre\CalDAV\Backend\AbstractBackend implements
             $this->eventEmitter,
             $this->propertyMap,
             $this->server ?? null
+        );
+        $this->calendarObjectService = new CalendarObjectService(
+            $this->calendarObjectDAO,
+            $this->calendarDataNormalizer
         );
 
         $this->ensureIndex();
@@ -150,311 +156,41 @@ class Mongo extends \Sabre\CalDAV\Backend\AbstractBackend implements
 
     function getCalendarObjects($calendarId) {
         $this->_assertIsArray($calendarId);
-
-        $calendarId = $calendarId[0];
-
-        $projection = [
-            '_id' => 1,
-            'uri' => 1,
-            'lastmodified' => 1,
-            'etag' => 1,
-            'calendarid' => 1,
-            'size' => 1,
-            'componenttype' => 1
-        ];
-
-        $result = [];
-        foreach ($this->calendarObjectDAO->findByCalendarId($calendarId, $projection) as $row) {
-            $result[] = [
-                'id'           => (string) $row['_id'],
-                'uri'          => $row['uri'],
-                'lastmodified' => $row['lastmodified'],
-                'etag'         => '"' . $row['etag'] . '"',
-                'size'         => (int) $row['size'],
-                'component'    => strtolower($row['componenttype']),
-            ];
-        }
-
-        return $result;
+        return $this->calendarObjectService->getCalendarObjects($calendarId);
     }
 
     function getCalendarObject($calendarId, $objectUri) {
-        $result = $this->getMultipleCalendarObjects($calendarId, [ $objectUri ]);
-
-        return array_shift($result);
+        return $this->calendarObjectService->getCalendarObject($calendarId, $objectUri);
     }
 
     function getMultipleCalendarObjects($calendarId, array $uris) {
         $this->_assertIsArray($calendarId);
-
-        $calendarId = $calendarId[0];
-
-        $projection = [
-            '_id' => 1,
-            'uri' => 1,
-            'lastmodified' => 1,
-            'etag' => 1,
-            'calendarid' => 1,
-            'size' => 1,
-            'calendardata' => 1,
-            'componenttype' => 1
-        ];
-
-        $result = [];
-        foreach ($this->calendarObjectDAO->findByCalendarIdAndUris($calendarId, $uris, $projection) as $row) {
-            $result[] = [
-                'id'           => (string) $row['_id'],
-                'uri'          => $row['uri'],
-                'lastmodified' => $row['lastmodified'],
-                'etag'         => '"' . $row['etag'] . '"',
-                'size'         => (int) $row['size'],
-                'calendardata' => $row['calendardata'],
-                'component'    => strtolower($row['componenttype']),
-            ];
-        }
-
-        return $result;
+        return $this->calendarObjectService->getMultipleCalendarObjects($calendarId, $uris);
     }
 
     function createCalendarObject($calendarId, $objectUri, $calendarData) {
         $this->_assertIsArray($calendarId);
-
-        $calendarId = $calendarId[0];
-
-        $extraData = $this->getDenormalizedData($calendarData);
-
-        $obj = [
-            'calendarid' => $calendarId,
-            'uri' => $objectUri,
-            'calendardata' => $calendarData,
-            'lastmodified' => time(),
-            'etag' => $extraData['etag'],
-            'size' => $extraData['size'],
-            'componenttype' => $extraData['componentType'],
-            'firstoccurence' => $extraData['firstOccurence'],
-            'lastoccurence' => $extraData['lastOccurence'],
-            'uid' => $extraData['uid']
-        ];
-        $this->calendarObjectDAO->createCalendarObject($obj);
-        $this->addChange($calendarId, $objectUri, 1);
-
-        return '"' . $extraData['etag'] . '"';
+        return $this->calendarObjectService->createCalendarObject($calendarId, $objectUri, $calendarData, [$this, 'addChange']);
     }
 
     function updateCalendarObject($calendarId, $objectUri, $calendarData) {
         $this->_assertIsArray($calendarId);
-
-        $calendarId = $calendarId[0];
-
-        $extraData = $this->getDenormalizedData($calendarData);
-
-        $updateData = [
-            'calendardata' => $calendarData,
-            'lastmodified' => time(),
-            'etag' => $extraData['etag'],
-            'size' => $extraData['size'],
-            'componenttype' => $extraData['componentType'],
-            'firstoccurence' => $extraData['firstOccurence'],
-            'lastoccurence' => $extraData['lastOccurence'],
-            'uid' => $extraData['uid'],
-        ];
-
-        $this->calendarObjectDAO->updateCalendarObject($calendarId, $objectUri, $updateData);
-        $this->addChange($calendarId, $objectUri, 2);
-
-        return '"' . $extraData['etag'] . '"';
+        return $this->calendarObjectService->updateCalendarObject($calendarId, $objectUri, $calendarData, [$this, 'addChange']);
     }
 
     function deleteCalendarObject($calendarId, $objectUri) {
         $this->_assertIsArray($calendarId);
-
-        $calendarId = $calendarId[0];
-
-        $this->calendarObjectDAO->deleteCalendarObject($calendarId, $objectUri);
-        $this->addChange($calendarId, $objectUri, 3);
+        $this->calendarObjectService->deleteCalendarObject($calendarId, $objectUri, [$this, 'addChange']);
     }
 
     function calendarQuery($calendarId, array $filters) {
-        $result = [];
-        foreach ($this->executeCalendarQuery($calendarId, $filters, false) as $item) {
-            $result[] = $item['uri'];
-        }
-        return $result;
-    }
-
-    /**
-     * Optimized version of calendarQuery that returns full object data (uri, calendardata, etag)
-     * instead of just URIs. This avoids the need for a subsequent getPropertiesForMultiplePaths call.
-     *
-     * @param mixed $calendarId
-     * @param array $filters
-     * @return array Array of objects with 'uri', 'calendardata', and 'etag' keys
-     */
-    function calendarQueryWithAllData($calendarId, array $filters) {
-        return $this->executeCalendarQuery($calendarId, $filters, true);
-    }
-
-    /**
-     * Execute calendar query with optional full data return
-     *
-     * @param mixed $calendarId [calendarId, instanceId]
-     * @param array $filters CalDAV filters
-     * @param bool $returnFullData If true, yields full data; if false, yields uri only
-     * @return \Generator Yields calendar objects
-     */
-    private function executeCalendarQuery($calendarId, array $filters, $returnFullData) {
         $this->_assertIsArray($calendarId);
-        $calendarId = $calendarId[0];
-
-        list($query, $projection, $requirePostFilter) =
-            $this->buildQueryFromFilters($calendarId, $filters, $returnFullData);
-
-        foreach ($this->calendarObjectDAO->findWithQuery($query, $projection) as $row) {
-            $result = $this->processQueryResult($row, $filters, $requirePostFilter, $returnFullData);
-
-            if ($result !== null) {
-                yield $result;
-            }
-        }
+        return $this->calendarObjectService->calendarQuery($calendarId, $filters);
     }
 
-    /**
-     * Build MongoDB query and projection from CalDAV filters
-     *
-     * @param string $calendarId
-     * @param array $filters
-     * @param bool $returnFullData
-     * @return array [query, projection, requirePostFilter]
-     */
-    private function buildQueryFromFilters($calendarId, array $filters, $returnFullData) {
-        $query = ['calendarid' => $calendarId];
-        $requirePostFilter = true;
-
-        // if no filters were specified, we don't need to filter after a query
-        if (empty($filters['prop-filters']) && empty($filters['comp-filters'])) {
-            $requirePostFilter = false;
-        }
-
-        list($componentType, $timeRange, $requirePostFilter) =
-            $this->extractComponentFilters($filters, $requirePostFilter);
-
-        if ($componentType) {
-            $query['componenttype'] = $componentType;
-        }
-
-        if ($timeRange && $timeRange['start']) {
-            $query['lastoccurence'] = ['$gte' => $timeRange['start']->getTimeStamp()];
-        }
-        if ($timeRange && $timeRange['end']) {
-            $query['firstoccurence'] = ['$lt' => $timeRange['end']->getTimeStamp()];
-        }
-
-        $projection = $this->buildProjection($requirePostFilter, $returnFullData);
-
-        return [$query, $projection, $requirePostFilter];
-    }
-
-    /**
-     * Extract component type and time range from filters
-     *
-     * @param array $filters
-     * @param bool $requirePostFilter
-     * @return array [componentType, timeRange, requirePostFilter]
-     */
-    private function extractComponentFilters(array $filters, $requirePostFilter) {
-        $componentType = null;
-        $timeRange = null;
-
-        // Figuring out if there's a component filter
-        if (!empty($filters['comp-filters']) && is_array($filters['comp-filters']) && !$filters['comp-filters'][0]['is-not-defined']) {
-            $componentType = $filters['comp-filters'][0]['name'];
-
-            // Checking if we need post-filters
-            if (empty($filters['prop-filters']) && empty($filters['comp-filters'][0]['comp-filters']) && empty($filters['comp-filters'][0]['time-range']) && empty($filters['comp-filters'][0]['prop-filters'])) {
-                $requirePostFilter = false;
-            }
-
-            // There was a time-range filter
-            if ($componentType == 'VEVENT' && is_array($filters['comp-filters'][0]['time-range'])) {
-                $timeRange = $filters['comp-filters'][0]['time-range'];
-
-                // If start time OR the end time is not specified, we can do a 100% accurate query
-                if (empty($filters['prop-filters']) && empty($filters['comp-filters'][0]['comp-filters']) && empty($filters['comp-filters'][0]['prop-filters']) && (empty($timeRange['start']) || empty($timeRange['end']))) {
-                    $requirePostFilter = false;
-                }
-            }
-        }
-
-        return [$componentType, $timeRange, $requirePostFilter];
-    }
-
-    /**
-     * Build projection based on requirements
-     *
-     * @param bool $requirePostFilter
-     * @param bool $returnFullData
-     * @return array Projection array
-     */
-    private function buildProjection($requirePostFilter, $returnFullData) {
-        if ($returnFullData) {
-            return ['uri' => 1, 'calendardata' => 1, 'etag' => 1];
-        }
-
-        if ($requirePostFilter) {
-            return ['uri' => 1, 'calendardata' => 1];
-        }
-
-        return ['uri' => 1];
-    }
-
-    /**
-     * Process a single query result row
-     *
-     * @param array $row Database row
-     * @param array $filters CalDAV filters
-     * @param bool $requirePostFilter Whether post-filtering is needed
-     * @param bool $returnFullData Whether to return full data
-     * @return array|null Processed result or null if filtered out
-     */
-    private function processQueryResult($row, $filters, $requirePostFilter, $returnFullData) {
-        $vObject = null;
-
-        if ($requirePostFilter) {
-            $vObject = VObject\Reader::read($row['calendardata']);
-
-            if (!$this->validateFilterForObjectWithVObject($vObject, $filters)) {
-                $vObject->destroy();
-                return null;
-            }
-        }
-
-        if ($returnFullData) {
-            return [
-                'uri' => $row['uri'],
-                'calendardata' => $row['calendardata'],
-                'etag' => '"' . $row['etag'] . '"',
-                'vObject' => $vObject
-            ];
-        }
-
-        if ($vObject) {
-            $vObject->destroy();
-        }
-
-        return ['uri' => $row['uri']];
-    }
-
-    /**
-     * Optimized version of validateFilterForObject that accepts a pre-parsed VObject.
-     * This avoids re-parsing the calendar data when the VObject is already available.
-     *
-     * @param VObject\Component\VCalendar $vObject
-     * @param array $filters
-     * @return bool
-     */
-    protected function validateFilterForObjectWithVObject($vObject, array $filters) {
-        $validator = new \Sabre\CalDAV\CalendarQueryValidator();
-        return $validator->validate($vObject, $filters);
+    function calendarQueryWithAllData($calendarId, array $filters) {
+        $this->_assertIsArray($calendarId);
+        return $this->calendarObjectService->calendarQueryWithAllData($calendarId, $filters);
     }
 
     function getCalendarObjectByUID($principalUri, $uid) {
@@ -615,7 +351,7 @@ class Mongo extends \Sabre\CalDAV\Backend\AbstractBackend implements
         return $this->calendarDataNormalizer->getDenormalizedData($calendarData);
     }
 
-    protected function addChange($calendarId, $objectUri, $operation) {
+    public function addChange($calendarId, $objectUri, $operation) {
         $res = $this->calendarDAO->getSyncToken($calendarId);
 
         if (!$res) {

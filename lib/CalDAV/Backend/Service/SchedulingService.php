cfd9316f381448a2701a3e06930e84b303be12fa
Benoit TELLIER <btellier@linagora.com>
2025-12-09 21:07:22 +0100

    SchedulingService: share mapping layer

--- lib/CalDAV/Backend/Service/SchedulingService.php
+++ lib/CalDAV/Backend/Service/SchedulingService.php
@@ -12,6 +12,14 @@ use ESN\CalDAV\Backend\DAO\SchedulingObjectDAO;
  * - Retrieving scheduling objects for a principal
  */
 class SchedulingService {
+    const PROJECTION = [
+        'uri' => 1,
+        'calendardata' => 1,
+        'lastmodified' => 1,
+        'etag' => 1,
+        'size' => 1
+    ];
+
     private $schedulingObjectDAO;
 
     public function __construct(SchedulingObjectDAO $schedulingObjectDAO) {
@@ -19,20 +27,12 @@ class SchedulingService {
     }
 
     /**
-     * Get a single scheduling object
+     * Transform MongoDB row to domain object
      *
-     * @param string $principalUri
-     * @param string $objectUri
-     * @return array|null Scheduling object data or null if not found
+     * @param array $row MongoDB document
+     * @return array Domain object
      */
-    public function getSchedulingObject($principalUri, $objectUri) {
-        $projection = ['uri' => 1, 'calendardata' => 1, 'lastmodified' => 1, 'etag' => 1, 'size' => 1];
-        $row = $this->schedulingObjectDAO->findByPrincipalUriAndUri($principalUri, $objectUri, $projection);
-
-        if (!$row) {
-            return null;
-        }
-
+    private function asDomainObject($row) {
         return [
             'uri'          => $row['uri'],
             'calendardata' => $row['calendardata'],
@@ -43,23 +43,52 @@ class SchedulingService {
     }
 
     /**
+     * Transform domain data to MongoDB document
+     *
+     * @param string $principalUri Principal URI
+     * @param string $objectUri Object URI
+     * @param string $objectData iCalendar data
+     * @return array MongoDB document
+     */
+    private function asDocument($principalUri, $objectUri, $objectData) {
+        return [
+            'principaluri' => $principalUri,
+            'calendardata' => $objectData,
+            'uri' => $objectUri,
+            'lastmodified' => time(),
+            'etag' => md5($objectData),
+            'size' => strlen($objectData),
+            'dateCreated' => new \MongoDB\BSON\UTCDateTime(time() * 1000)
+        ];
+    }
+
+    /**
+     * Get a single scheduling object
+     *
+     * @param string $principalUri
+     * @param string $objectUri
+     * @return array|null Scheduling object data or null if not found
+     */
+    public function getSchedulingObject($principalUri, $objectUri) {
+        $row = $this->schedulingObjectDAO->findByPrincipalUriAndUri($principalUri, $objectUri, self::PROJECTION);
+
+        if (!$row) {
+            return null;
+        }
+
+        return $this->asDomainObject($row);
+    }
+
+    /**
      * Get all scheduling objects for a principal
      *
      * @param string $principalUri
      * @return array Array of scheduling objects
      */
     public function getSchedulingObjects($principalUri) {
-        $projection = ['uri' => 1, 'calendardata' => 1, 'lastmodified' => 1, 'etag' => 1, 'size' => 1];
-
         $result = [];
-        foreach($this->schedulingObjectDAO->findByPrincipalUri($principalUri, $projection) as $row) {
-            $result[] = [
-                'calendardata' => $row['calendardata'],
-                'uri'          => $row['uri'],
-                'lastmodified' => $row['lastmodified'],
-                'etag'         => '"' . $row['etag'] . '"',
-                'size'         => (int) $row['size'],
-            ];
+        foreach($this->schedulingObjectDAO->findByPrincipalUri($principalUri, self::PROJECTION) as $row) {
+            $result[] = $this->asDomainObject($row);
         }
 
         return $result;
@@ -83,16 +112,7 @@ class SchedulingService {
      * @param string $objectData
      */
     public function createSchedulingObject($principalUri, $objectUri, $objectData) {
-        $obj = [
-            'principaluri' => $principalUri,
-            'calendardata' => $objectData,
-            'uri' => $objectUri,
-            'lastmodified' => time(),
-            'etag' => md5($objectData),
-            'size' => strlen($objectData),
-            'dateCreated' => new \MongoDB\BSON\UTCDateTime(time() * 1000)
-        ];
-
-        $this->schedulingObjectDAO->createSchedulingObject($obj);
+        $document = $this->asDocument($principalUri, $objectUri, $objectData);
+        $this->schedulingObjectDAO->createSchedulingObject($document);
     }
 }

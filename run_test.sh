86fe01e52d99d9c7fb8c89613f5523b5cd337328
Hung <hphan@linagora.com>
2026-03-31 16:34:14 +0700

    [NO_MERGE] Test with new integration test

--- run_test.sh
+++ run_test.sh
@@ -63,6 +63,7 @@ fi
 (
   git clone https://github.com/linagora/twake-calendar-integration-tests.git it-tests
   cd it-tests
+  git checkout Test-admin-modify-resource-events-when-subscribing-again
   bash pre-build.sh esn_sabre_test
   mvn clean install -Dapi.version=1.43 -Dtest=com.linagora.dav.sabrev4_7.**
 )

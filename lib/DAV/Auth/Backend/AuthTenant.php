58c29cb6db407dc6c576ee094193dc392157d072
Bastien Roucariès <rouca@debian.org>
2026-05-16 00:07:23 +0200

    Use an AuthTenant object for authentification
    Allow better granularity will be later emited

---
+++ lib/DAV/Auth/Backend/AuthTenant.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ESN\DAV\Auth\Backend;
+
+class AuthTenant
+{
+    public readonly TenantType $tenantType;
+    public readonly string $userId;
+
+    protected $prefixes = [
+        TenantType::User->value => 'principals/users/',
+        TenantType::Technical->value => 'principals/users/',
+        TenantType::Resources->value => 'principals/resources/'
+    ];
+
+    public function __construct(
+        string $userId,
+        TenantType $tenantType = TenantType::User
+    ) {
+        $this->tenantType = $tenantType;
+        $this->userId = $userId;
+    }
+
+    public function getPrincipal(): Principal {
+        return new Principal($this->prefixes[$this->tenantType->value], $this->userId);
+    }
+}

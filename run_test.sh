24e5c00aefd9e79d19c37b07597931f288646b1c
Benoit TELLIER <btellier@linagora.com>
2026-06-10 07:23:02 +0200

    Turn on async scheduling in IT tests (#383)

--- run_test.sh
+++ run_test.sh
@@ -58,7 +58,7 @@ javatest() {
     git clone https://github.com/linagora/twake-calendar-integration-tests.git it-tests ||  exit 1
     cd it-tests || exit 1
     bash pre-build.sh esn_sabre_test || exit 1
-    mvn clean install -Dapi.version=1.43 -Dtest=com.linagora.dav.sabrev4_7.** || exit 1
+    mvn clean install -Dapi.version=1.43 -Dtest=com.linagora.dav.sabrev4_7.** -Damqp.scheduling.enabled=true || exit 1
 }
 
 # Pre Cleanup

59822f6082245b66ceb09929b3ac6e49dca68ab9
Benoit TELLIER <btellier@linagora.com>
2026-07-07 17:28:29 +0200

    TCALENDAR-925 Event counter is broken on imap.linagora.com

--- tests/DAV/XHttpMethodOverridePluginTest.php
+++ tests/DAV/XHttpMethodOverridePluginTest.php
@@ -31,6 +31,24 @@ class XHttpMethodOverridePluginTest extends \PHPUnit\Framework\TestCase {
         $this->assertEquals($response->getBodyAsString(), 'OK');
     }
 
+    function testMethodOverrideExposesOverriddenMethodToDownstreamPlugins() {
+        list($corsplugin, $server) = $this->prepareServer();
+
+        $itipPlugin = $server->getPlugin('MockItipPlugin');
+        $originalRequest = $server->httpRequest;
+        $originalRequest->setMethod("POST");
+        $originalRequest->setHeader("X-Http-Method-Override", "ITIP");
+
+        $server->invokeMethod($server->httpRequest, $server->httpResponse);
+
+        // Downstream handlers must observe the effective ITIP method on the
+        // server request, not the original POST.
+        $this->assertEquals("ITIP", $itipPlugin->observedServerMethod);
+        // The original request is restored once the override sub-call returns.
+        $this->assertSame($originalRequest, $server->httpRequest);
+        $this->assertEquals("POST", $server->httpRequest->getMethod());
+    }
+
     function testStandardWithoutMethodOverride() {
         list($corsplugin, $server) = $this->prepareServer();
 
@@ -55,12 +73,19 @@ class XHttpMethodOverridePluginTestSapiMock {
 #[\AllowDynamicProperties]
 class MockItipPlugin extends \Sabre\DAV\ServerPlugin {
 
+    public $observedServerMethod;
+
+    function getPluginName() {
+        return 'MockItipPlugin';
+    }
+
     function initialize(\Sabre\DAV\Server $server) {
         $this->server = $server;
         $this->server->on('method:ITIP', [$this, 'send'], 90);
     }
 
     function send(\Sabre\HTTP\RequestInterface $request, \Sabre\HTTP\ResponseInterface $response) {
+        $this->observedServerMethod = $this->server->httpRequest->getMethod();
         $this->server->httpResponse->setBody("OK");
         $this->server->httpResponse->setStatus(200);
         return false;
